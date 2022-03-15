982cefd2473dbaa42bd5d13a9e141c3dc3af05ae
Alexandre <alexandre@weel.com>
2022-03-15 09:11:16 +0200

    added table - style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,67 @@
 import react from 'react';
 import { useEffect, useState } from "react";
 
-////////////////////////////////////////
-
-
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Home = (props) => {
 
-const [customers, setCustomers] = useState([]);    
-    
-useEffect(() => {
-  fetch('http://localhost:5000/api/customers')
-    .then(res => res.json())
-    .then(data => {
-      setCustomers(data);
-    })
-}, []);
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+            backgroundColor: theme.palette.common.black,
+            color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+            fontSize: 14,
+        },
+    }));
 
+    const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+            border: 0,
+        },
+    }));
 
-return (
-  <div className="App">
-    <ul>
-      {customers.map(customer => (
-        <li key={customer.id}>
-          {customer.firstName} {customer.lastName}
-        </li>
-      ))}
-    </ul>
-  </div>
- );
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/customers')
+            .then(res => res.json())
+            .then(data => {
+                setCustomers(data);
+            })
+    }, []);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell align='center' >First Name</StyledTableCell>
+                        <StyledTableCell align='center'>Last Name</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {customers.map((customer) => (
+                        <StyledTableRow key={customer.id}>
+                            <StyledTableCell align='center'>{customer.firstName}</StyledTableCell>
+                            <StyledTableCell align='center'>{customer.lastName}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 };
 
 export default Home
